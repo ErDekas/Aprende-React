09f8224dbcc02f4736b4c193a11479cdda08cc03
ErDekas <pabletor0505@gmail.com>
2024-11-11 12:58:33 +0100

    Arreglo carpetas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Nivel-2-Intermedio/ejercicio-10/App';
+import App from './Nivel-2-Intermedio/Ejercicio-10/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
